9cbd6eb9daca71817473aa98177d9f8d0927a0f8
castaneai <castaneai@castaneai.net>
2015-10-05 16:47:46 +0900

    投稿日時を削除、URLを文字列に
    たいていサーバーからURL情報を取る時は最初文字列で来るし、url.Parseだとerrのハンドリングが面倒なので
    url.URLオブジェクトではなくstringにした
    投稿日時については特に存在理由がないので削除した

--- src/kirara/resource.go
+++ src/kirara/resource.go
@@ -1,18 +1,12 @@
 package kirara
 
-import (
-	"net/url"
-	"time"
-)
-
 type Tag string
 
 type Post struct {
 	PostID string
-	ImageURLs []url.URL
-	ThumbnailImageURL url.URL
+	ImageURLs []string
+	ThumbnailImageURL string
 	Tags []Tag
-	CreatedAt time.Time
 }
 
 type Resource interface {
