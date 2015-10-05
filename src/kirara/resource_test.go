fa9d8f6ee4381eddaa6900e112ce86091e460542
castaneai <castaneai@castaneai.net>
2015-10-05 16:48:03 +0900

    Tiqavリソースを追加

--- src/kirara/resource_test.go
+++ src/kirara/resource_test.go
@@ -4,35 +4,13 @@ import (
 	"testing"
 )
 
-type testResource struct {}
-
-func (t *testResource) ID() string {
-	return "test-resource"
-}
-
-func (t *testResource) Name() string {
-	return "Test Resource"
-}
-
-func (t *testResource) SearchByKeyword(keyword string, pageNumber int) ([]Post, error) {
-	return []Post {}, nil
-}
-
-func (t *testResource) SearchByTags(tags []Tag, pageNumber int) ([]Post, error) {
-	return []Post {}, nil
-}
-
-func (t *testResource) AutoComplete(input string) ([]Tag, error) {
-	return []Tag {}, nil
-}
-
-func TestInterface(t *testing.T) {
-	var r Resource
-	r = &testResource{}
-	if r.ID() != "test-resource" {
-		t.Errorf("リソースIDが不正")
+func TestTiqav(t *testing.T) {
+	r := Tiqav {}
+	posts, err := r.SearchByKeyword("ちくわ", 1)
+	if err != nil {
+		t.Errorf("search by keyword failed.")
 	}
-	if r.Name() != "Test Resource" {
-		t.Errorf("リソース名が不正")
+	for i, post := range posts {
+		t.Logf("%d: %v", i, post)
 	}
 }
---
+++ src/kirara/resource_tiqav.go
@@ -0,0 +1,64 @@
+package kirara
+
+import (
+	"github.com/franela/goreq"
+	"fmt"
+)
+
+type Tiqav struct {}
+
+func (t *Tiqav) ID() string {
+	return "tiqav"
+}
+
+func (t *Tiqav) Name() string {
+	return "Tiqav"
+}
+
+type searchResponseData struct {
+	Id string
+	Ext string
+	SourceUrl string
+}
+
+func getPostTags(postID string) ([]Tag, error) {
+	res, err := goreq.Request{Uri: fmt.Sprintf("http://api.tiqav.com/images/%s/tags.json", postID)}.Do()
+	if err != nil {
+		return nil, err
+	}
+	var tags []Tag
+	res.Body.FromJsonTo(&tags)
+	return tags, nil
+}
+
+func (t *Tiqav) SearchByKeyword(keyword string, pageNumber int) ([]Post, error) {
+	res, err := goreq.Request{Uri: fmt.Sprintf("http://api.tiqav.com/search.json?q=%s", keyword)}.Do()
+	if err != nil {
+		return nil, err
+	}
+
+	var data []searchResponseData
+	res.Body.FromJsonTo(&data)
+	var posts []Post
+	for _, d := range data {
+		tags, err := getPostTags(d.Id)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, Post {
+			PostID: d.Id,
+			ImageURLs: []string {fmt.Sprintf("http://img.tiqav.com/%s.%s", d.Id, d.Ext)},
+			ThumbnailImageURL: fmt.Sprintf("http://img.tiqav.com/%s.th.jpg", d.Id),
+			Tags: tags,
+		})
+	}
+	return posts, nil
+}
+
+func (t *Tiqav) SearchByTags(tags []Tag, pageNumber int) ([]Post, error) {
+	return []Post {}, nil
+}
+
+func (t *Tiqav) AutoComplete(input string) ([]Tag, error) {
+	return []Tag{}, nil
+}
